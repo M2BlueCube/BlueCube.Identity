0ce3ced4ea12eb045a9618779524ea927b906975
Morteza Asghari <morteza.asghari@live.com>
2023-09-30 23:47:04 +0200

    Delete secrets

--- BlueCube.Identity/Program.cs
+++ BlueCube.Identity/Program.cs
@@ -12,8 +12,7 @@ var services = builder.Services;
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
                        ?? throw new KeyNotFoundException(" DefaultConnection is not found in Configuration");
 
-services.AddDbContext<IdentityDbContext>(options => options.UseNpgsql(connectionString, 
-        b=> b.MigrationsAssembly("BlueCube.Identity") ));
+services.AddDbContext<IdentityDbContext>(options => options.UseNpgsql(connectionString, b => b.MigrationsAssembly("BlueCube.Identity")));
 
 services.AddIdentity<IdentityUser,IdentityRole>()
     .AddEntityFrameworkStores<IdentityDbContext>()
